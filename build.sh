8df057ccd5fb8a36d548f3d3706c8b8d2607c83e
macdfree <macdfree@163.com>
2018-03-15 22:47:28 +0800

    travis ci test04

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -e
 
-echo $TRAVIS_TAG
-#GOARCH=amd64 GOOS=linux go build -o bin/certstrap-${BUILD_TAG}-linux-amd64 ${REPO_PATH}
+GOARCH=amd64 GOOS=linux go build -o bin/sshs-linux-amd64 ./
