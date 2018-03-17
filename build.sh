aaf1962522cc13e3c6ccb0c6ad169941cfa8244f
macdfree <macdfree@163.com>
2018-03-17 17:18:58 +0800

    修改构建脚本

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -e
 
-GOARCH=amd64 GOOS=linux go build -o bin/sshs-linux-amd64 ./
+ORG_PATH="github.com/MACDfree"
+REPO_PATH="${ORG_PATH}/sshs"
+BUILD_TAG="${TRAVIS_TAG:-$(git describe --all | sed -e's/.*\///g')}"
+
+export GOPATH=$HOME/gopath
+export GOBIN=$GOPATH/bin
+
+GOARCH=amd64 GOOS=linux go build -o bin/sshs-${BUILD_TAG}-linux-amd64 ${REPO_PATH}
+GOARCH=amd64 GOOS=darwin go build -o bin/sshs-${BUILD_TAG}-darwin-amd64 ${REPO_PATH}
